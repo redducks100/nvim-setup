e28a57284787e9a8b301ca7d6f526daeabeffa1b
Simone Andronache <simoneandronache14@gmail.com>
2024-05-21 00:59:51 +0300

    try to configure debugging for rust

--- lua/shimu/plugins/lsp.lua
+++ lua/shimu/plugins/lsp.lua
@@ -164,6 +164,7 @@ return {
         -- gopls = {},
         -- pyright = {},
         rust_analyzer = {},
+        codelldb = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
