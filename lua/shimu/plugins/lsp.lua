fbe759632a6211a615c607617b052a923789a015
Simone Andronache <simoneandronache14@gmail.com>
2024-05-21 18:48:22 +0300

    Add debugging + testing + enable inlay hints

--- lua/shimu/plugins/lsp.lua
+++ lua/shimu/plugins/lsp.lua
@@ -6,7 +6,53 @@ return {
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
-      'simrat39/rust-tools.nvim',
+      {
+        'mrcjkb/rustaceanvim',
+        version = '^4',
+        ft = { 'rust' },
+        opts = {
+          server = {
+            on_attach = function(_, bufnr)
+              vim.lsp.inlay_hint.enable(true)
+              vim.keymap.set('n', '<leader>cR', function()
+                vim.cmd.RustLsp 'codeAction'
+              end, { desc = 'Code Action', buffer = bufnr })
+              vim.keymap.set('n', '<leader>dr', function()
+                vim.cmd.RustLsp 'debuggables'
+              end, { desc = 'Rust Debuggables', buffer = bufnr })
+            end,
+            default_settings = {
+              -- rust-analyzer language server configuration
+              ['rust-analyzer'] = {
+                cargo = {
+                  allFeatures = true,
+                  loadOutDirsFromCheck = true,
+                  buildScripts = {
+                    enable = true,
+                  },
+                },
+                -- Add clippy lints for Rust.
+                checkOnSave = {
+                  allFeatures = true,
+                  command = 'clippy',
+                  extraArgs = { '--no-deps' },
+                },
+                procMacro = {
+                  enable = true,
+                  ignored = {
+                    ['async-trait'] = { 'async_trait' },
+                    ['napi-derive'] = { 'napi' },
+                    ['async-recursion'] = { 'async_recursion' },
+                  },
+                },
+              },
+            },
+          },
+        },
+        config = function(_, opts)
+          vim.g.rustaceanvim = vim.tbl_deep_extend('keep', vim.g.rustaceanvim or {}, opts or {})
+        end,
+      },
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
@@ -19,8 +65,7 @@ return {
       inlay_hints = { enabled = true },
     },
     setup = {
-      rust_analyzer = function(_, opts)
-        require('rust-tools').setup { server = opts }
+      rust_analyzer = function()
         return true
       end,
       tailwindcss = function(_, opts)
@@ -163,8 +208,22 @@ return {
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        rust_analyzer = {},
         codelldb = {},
+        taplo = {
+          keys = {
+            {
+              'K',
+              function()
+                if vim.fn.expand '%:t' == 'Cargo.toml' and require('crates').popup_available() then
+                  require('crates').show_popup()
+                else
+                  vim.lsp.buf.hover()
+                end
+              end,
+              desc = 'Show Crate Documentation',
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
