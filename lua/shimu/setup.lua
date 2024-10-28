56c55604c97008c5336a6ee6a1e782f269a1f8c5
Simone Andronache <simoneandronache14@gmail.com>
2024-10-28 17:48:34 +0200

    push latest

--- lua/shimu/setup.lua
+++ lua/shimu/setup.lua
@@ -8,16 +8,16 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = vim.env.HOME .. "/.vim/undodir"
+vim.opt.undodir = vim.env.HOME .. '/.vim/undodir'
 vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
+vim.opt.signcolumn = 'yes'
+vim.opt.isfname:append '@-@'
+vim.opt.updatetime = 750
+vim.opt.colorcolumn = '80'
 
 vim.g.have_nerd_font = true
 
